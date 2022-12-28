a37f048cb27d7374f3241c8be2d2513e07ba6f04
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-12-28 16:42:51 -0300

    Paint cell when hovering with mouse

--- js/create-divs.js
+++ js/create-divs.js
@@ -1,14 +1,18 @@
 function createDivs(container, rows, cols) {
-    // Creates divs for rows x cols grid
+    /* Creates divs for a rowsXcols grid and returns an array
+    containing the created divs */
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-
+    let cells = [];
     for(let ind = 0; ind < rows*cols; ind++) {
         let div = document.createElement("div");
         div.style.cssText = `border: 0.1rem solid black`;
         div.classList.add("grid-item");
+        cells.push(div);
         container.appendChild(div);
     }
+
+    return cells;
 }
 
 export {createDivs}
--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ import { createDivs } from "./create-divs.js";
 
 const containerDiv = document.querySelector("#grid");
 
-createDivs(containerDiv, 25, 25);
+let cells = createDivs(containerDiv, 25, 25);
+let draw = true; // flag which indicates if the event listener is in color mode
+cells.forEach(cell => {
+    cell.addEventListener("mouseover", () => {
+        if(draw) {
+            cell.style.backgroundColor = 'blue';
+        } else {
+            cell.style.backgroundColor = 'white';
+        }
+    });
+});

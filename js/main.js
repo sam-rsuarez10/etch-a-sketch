edfc56af767e65aaa7b0bab5ca68e9ec023763d2
sam-rsuarez10 <suarezzcode2001@gmail.com>
2023-01-01 19:26:27 -0300

    Develop draw button funcionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 import { createDivs } from "./create-divs.js";
 
+// HTML elements selection
 const containerDiv = document.querySelector("#grid");
-let cells = createDivs(containerDiv, 16, 16);
-let draw = true; // flag which indicates if the event listener is in color mode
+const drawBtn = document.querySelector("#draw-btn");
 
+let cells = createDivs(containerDiv, 16, 16);
+let draw = false; // flag which indicates if the event listener is in color mode
+
+
+drawBtn.addEventListener('click', () => draw = true);
 
 cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {

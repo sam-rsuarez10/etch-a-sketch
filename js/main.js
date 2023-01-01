f0988443186a3906977ed7578c1a762b5d1e85fe
sam-rsuarez10 <suarezzcode2001@gmail.com>
2023-01-01 19:29:46 -0300

    Develop erase button funcionality

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ import { createDivs } from "./create-divs.js";
 // HTML elements selection
 const containerDiv = document.querySelector("#grid");
 const drawBtn = document.querySelector("#draw-btn");
+const eraseBtn = document.querySelector("#erase-btn");
 
+// Create grid
 let cells = createDivs(containerDiv, 16, 16);
+
 let draw = false; // flag which indicates if the event listener is in color mode
 
-
+// Event Listeners
 drawBtn.addEventListener('click', () => draw = true);
+eraseBtn.addEventListener('click', () => draw = false);
 
 cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {

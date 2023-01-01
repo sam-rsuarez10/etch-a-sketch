40ffaf1708d5af276fe858ab58176e72122316aa
sam-rsuarez10 <suarezzcode2001@gmail.com>
2023-01-01 19:19:10 -0300

    Make initial hover functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 import { createDivs } from "./create-divs.js";
 
 const containerDiv = document.querySelector("#grid");
-
-let cells = createDivs(containerDiv, 25, 25);
+let cells = createDivs(containerDiv, 16, 16);
 let draw = true; // flag which indicates if the event listener is in color mode
+
+
 cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {
         if(draw) {
             cell.style.backgroundColor = 'blue';
         } else {
-            cell.style.backgroundColor = 'white';
+            cell.style.backgroundColor = '';
         }
     });
 });
+
+// how to detect is click is pressed down in javascript?

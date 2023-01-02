a5f421081756ef1d622e14628a84be91783810cd
sam-rsuarez10 <suarezzcode2001@gmail.com>
2023-01-02 15:25:21 -0300

    Develop clear button functionality

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { createDivs } from "./create-divs.js";
 const containerDiv = document.querySelector("#grid");
 const drawBtn = document.querySelector("#draw-btn");
 const eraseBtn = document.querySelector("#erase-btn");
+const clearBtn = document.querySelector("#clear-btn");
 
 // Create grid
 let cells = createDivs(containerDiv, 16, 16);
@@ -13,6 +14,10 @@ let draw = false; // flag which indicates if the event listener is in color mode
 // Event Listeners
 drawBtn.addEventListener('click', () => draw = true);
 eraseBtn.addEventListener('click', () => draw = false);
+clearBtn.addEventListener('click', () => {
+    cells.forEach(cell => cell.style.backgroundColor = '');
+});
+
 
 cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {

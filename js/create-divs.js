5c7b362af9aa20be43c7da95f78b2f41410f316e
sam-rsuarez10 <suarezzcode2001@gmail.com>
2023-01-03 12:24:10 -0300

    Develop grid button funcionality

--- js/create-divs.js
+++ js/create-divs.js
@@ -1,10 +1,10 @@
-function createDivs(container, rows, cols) {
-    /* Creates divs for a rowsXcols grid and returns an array
+function createDivs(container, scale) {
+    /* Creates divs for a scaleXscale grid and returns an array
     containing the created divs */
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+    container.style.setProperty('--grid-rows', scale);
+    container.style.setProperty('--grid-cols', scale);
     let cells = [];
-    for(let ind = 0; ind < rows*cols; ind++) {
+    for(let ind = 0; ind < scale*scale; ind++) {
         let div = document.createElement("div");
         div.style.cssText = `border: 0.05rem solid black;`;
         div.classList.add("grid-item");
--- js/main.js
+++ js/main.js
@@ -1,32 +1,60 @@
 import { createDivs } from "./create-divs.js";
 
+function listen_cells() {
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            if(draw) {
+                cell.style.backgroundColor = 'blue';
+            } else {
+                cell.style.backgroundColor = '';
+            }
+        });
+    });
+}
+
+let draw = false; // flag which indicates if the event listener is in color mode
+
 // HTML elements selection
 const containerDiv = document.querySelector("#grid");
 const drawBtn = document.querySelector("#draw-btn");
 const eraseBtn = document.querySelector("#erase-btn");
 const clearBtn = document.querySelector("#clear-btn");
+const gridBtn = document.querySelector("#grid-btn");
 
 // Create grid
-let cells = createDivs(containerDiv, 16, 16);
-
-let draw = false; // flag which indicates if the event listener is in color mode
+const maxScale = 70;
+let gridScale = 16; // rows * cols scale for grid
+let cells = createDivs(containerDiv, gridScale);
+listen_cells();
 
 // Event Listeners
+gridBtn.addEventListener('click', () => {
+    // ask user a number scale for grid
+    let validScale = false;
+    while (!validScale) {
+        let inputScale = parseInt(prompt("Type a rows X columns scale for sketchpad"));
+        if(inputScale && inputScale <= maxScale) {
+            validScale =  true;
+            // remove existing cells from grid
+            cells.forEach(cell => {
+                containerDiv.removeChild(cell);
+
+            });
+            // create cells with new scale
+            cells = createDivs(containerDiv, inputScale);
+            listen_cells();
+        }
+    }
+});
+
 drawBtn.addEventListener('click', () => draw = true);
+
 eraseBtn.addEventListener('click', () => draw = false);
+
 clearBtn.addEventListener('click', () => {
     cells.forEach(cell => cell.style.backgroundColor = '');
 });
 
 
-cells.forEach(cell => {
-    cell.addEventListener("mouseover", () => {
-        if(draw) {
-            cell.style.backgroundColor = 'blue';
-        } else {
-            cell.style.backgroundColor = '';
-        }
-    });
-});
 
 // how to detect is click is pressed down in javascript?
